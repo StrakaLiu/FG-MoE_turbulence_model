149ba3c17e7abe2836e10f573e98dfcce7b48b64
Hao-Chen Liu <liu.h.c@imech.ac.cn>
2026-04-27 21:27:33 +0800

    Add postProcess functions to run_cases0To6.sh

--- runTestCases/run_cases0To6.sh
+++ runTestCases/run_cases0To6.sh
@@ -4,9 +4,9 @@ cp -r ./initialCases/01_channel/ .
 cp ../requiredModules/* 01_channel/
 cd 01_channel
 PysimpleFoam
-postProcess -func sample_in
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sample_in
 cd ..
 
 cp -r ./initialCases/02_ZPGPlate/ .
@@ -15,10 +15,10 @@ cd 02_ZPGPlate
 decomposePar
 mpirun  -n 8 PysimpleFoam -parallel
 reconstructPar -latestTime
-postProcess -func sample_down
 rm -rf processor*
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sample_down
 cd ..
 
 cp -r ./initialCases/03_planeJet/ .
@@ -28,37 +28,37 @@ decomposePar
 mpirun -n 8 PyrhoSimpleFoam -parallel
 reconstructPar -latestTime
 rm -rf processor*
-postProcess -func sampleDict
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sampleDict
 cd ..
 
 cp -r ./initialCases/1_nasaHump/ .
 cp ../requiredModules/* 1_nasaHump/
 cd 1_nasaHump
 PysimpleFoam
-postProcess -func sample_down
-postProcess -func sample_lines_U
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sample_down
+postProcess -func sample_lines_U
 cd ..
 
 cp -r ./initialCases/2_sqrDuct_Re=40000/ .
 cp ../requiredModules/* 2_sqrDuct_Re=40000/
 cd 2_sqrDuct_Re=40000
 PysimpleFoam
-postProcess -func sample_left
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sample_left
 cd ..
 
 cp -r ./initialCases/2.1_recDuct/ .
 cp ../requiredModules/* 2.1_recDuct/
 cd 2.1_recDuct
 PysimpleFoam
-postProcess -func extractPlane
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func extractPlane
 cd ..
 
 cp -r ./initialCases/3_ASJ/ .
@@ -68,9 +68,9 @@ decomposePar
 mpirun -n 8 PyrhoSimpleFoam -parallel
 reconstructPar -latestTime
 rm -rf processor*
-postProcess -func sampleDict
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sampleDict
 cd ..
 
 cp -r ./initialCases/3.1_ANSJ/ .
@@ -80,36 +80,36 @@ decomposePar
 mpirun -n 8 PyrhoSimpleFoam -parallel
 reconstructPar -latestTime
 rm -rf processor*
-postProcess -func sampleDict
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sampleDict
 cd ..
 
 cp -r ./initialCases/4.2_NACA0012_AOA0/ .
 cp ../requiredModules/* 4.2_NACA0012_AOA0/
 cd 4.2_NACA0012_AOA0
 PysimpleFoam
-postProcess -func sampleDict
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sampleDict
 cd ..
 
 cp -r ./initialCases/4.1_NACA0012_AOA10/ .
 cp ../requiredModules/* 4.1_NACA0012_AOA10/
 cd 4.1_NACA0012_AOA10
 PysimpleFoam
-postProcess -func sampleDict
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sampleDict
 cd ..
 
 cp -r ./initialCases/4_NACA0012_AOA15/ .
 cp ../requiredModules/* 4_NACA0012_AOA15/
 cd 4_NACA0012_AOA15 
 PysimpleFoam
-postProcess -func sampleDict
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sampleDict
 cd ..
 
 cp -r ./initialCases/5_bump/ .
@@ -119,11 +119,11 @@ decomposePar
 mpirun -n 8 PysimpleFoam -parallel
 reconstructPar -latestTime
 rm -rf processor*
+postProcess -func writeCellCentres
+postProcess -func writeCellVolumes
 postProcess -func sample_down
 postProcess -func sample_lines_U9
 postProcess -func sample_left_U
-postProcess -func writeCellCentres
-postProcess -func writeCellVolumes
 cd ..
 
 cp -r ./initialCases/6_pehill/ .
@@ -133,11 +133,11 @@ decomposePar
 mpirun -n 4 PysimpleFoam -parallel
 reconstructPar -latestTime
 rm -rf processor*
+postProcess -func writeCellCentres
+postProcess -func writeCellVolumes
 postProcess -func 'sampleDict' -latestTime
 postProcess -func 'sample_down' -latestTime
 postProcess -func 'sample_left_U' -latestTime
-postProcess -func writeCellCentres
-postProcess -func writeCellVolumes
 cd ..
 
 
