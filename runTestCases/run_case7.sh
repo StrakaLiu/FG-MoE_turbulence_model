4a04a72fca1c3e85fad475c58c43f53f1363df68
Hao-Chen Liu <liu.h.c@imech.ac.cn>
2026-04-27 21:28:04 +0800

    Update postProcess functions in run_case7.sh

--- runTestCases/run_case7.sh
+++ runTestCases/run_case7.sh
@@ -8,7 +8,8 @@ decomposePar
 mpirun  -n 128 PysimpleFoam -parallel
 reconstructPar -latestTime
 rm -rf processor*
-postProcess -func sample_plane_z0
 postProcess -func writeCellCentres
 postProcess -func writeCellVolumes
+postProcess -func sample_plane_z0
+
 
